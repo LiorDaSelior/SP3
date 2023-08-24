920de3a6ec2065524e97825bc50a3b7344b0ebd9
LiorDaSelior <liorbelenkov@mail.tau.ac.il>
2023-08-24 03:07:55 +0300

    working c code + header
    -use functions in header for module
    -missing cmake file

---
+++ symnmf.h
@@ -0,0 +1,10 @@
+typedef struct {
+int row;
+int col;
+double* data;
+} Matrix;
+
+Matrix* create_sym_matrix(Matrix*);
+Matrix* create_ddg_matrix(Matrix*);
+Matrix* create_norm_matrix(Matrix*, Matrix*);
+Matrix* create_ass_matrix(Matrix*, Matrix*, double, int, double);
